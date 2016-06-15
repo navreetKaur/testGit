e7ee4bbfc8beb69528cd82f2a99f48b8eb1af329
navreetKaur <navreet.kaur10@gmail.com>
2016-06-15 15:52:27 -0400

    zork updated

--- src/Zork/Zork.java
+++ src/Zork/Zork.java
@@ -10,14 +10,14 @@ public class Zork {
 	public static void main(String[] args) {
 		String[][] zork = new String[3][3];
 
-		zork[0][0] = "5,dining room,dusty emptybox";
-		zork[0][1] = "6,vault,3 walking skeletons";
-		zork[0][2] = "7,parlor,treasurechest";
-		zork[1][0] = "3,library,spiders";
-		zork[1][1] = "2,front room,piano";
-		zork[1][2] = "4,kitchen,bats";
+		zork[0][0] = "5,dining room,dusty emptybox,50";
+		zork[0][1] = "6,vault,3 walking skeletons,100";
+		zork[0][2] = "7,parlor,treasurechest,600";
+		zork[1][0] = "3,library,spiders,800";
+		zork[1][1] = "2,front room,piano,0";
+		zork[1][2] = "4,kitchen,bats,1000";
 		zork[2][0] = "0";
-		zork[2][1] = "1,foyer,dead scorpion";
+		zork[2][1] = "1,foyer,dead scorpion,10";
 		zork[2][2] = "0";
 
 		Scanner in = new Scanner(System.in);
@@ -28,22 +28,33 @@ public class Zork {
 		int i = 1;
 		int j = 1;
 		boolean special = false;
-		while (true) {
+		int total = 0; 
+		int rooms = 1;
+		Random rnd = new Random();
+		int chor = rnd.nextInt(8) + 1;
+		while (!temp.equals("Q")) {
 			String vals[] = zork[i][j].split(",");
 			String currentRoom = vals[0];
+			if (currentRoom.equals(""+chor)) {
+				total =0;
+				System.out.println("You met chor and Lost all your winnings!");
+				chor = rnd.nextInt(8) + 1;
+			}
+			total += Integer.parseInt(vals[3]);
+			zork[i][j] = vals[0] + "," + vals[1] + "," + vals[2] + "," + "0";
 			if (!special) {
 				System.out.println("You are standing in the " + vals[1] + ".");
 				System.out.println("You see a " + vals[2] + ".");
 			} else {
 				System.out.println("You are standing in the secret room.");
 				System.out.println("You see piles of gold.");
+				special = false;
 			}
-			
+			System.out.println("Winnings so far: "+ total);
 			Map<String,String> options = new HashMap<String,String>();
 			System.out.print("{You can ");
 			int k=0;
 			if (currentRoom.equals("6")) {
-				Random rnd = new Random();
 				int randomInt = rnd.nextInt(4);
 				if (randomInt == 3) {
 					special = true;
@@ -95,7 +106,9 @@ public class Zork {
 
 			System.out.println(" or press Q to quit}");
 			temp = in.next();
+			
 			if (!temp.equals("Q") ) {
+				rooms++;
 				if (!currentRoom.equals("6")) {
 					String[] indexes = options.get(temp).split(",");
 					i = Integer.parseInt(indexes[0]);
@@ -108,6 +121,7 @@ public class Zork {
 
 		}
 		System.out.println("Sorry to see you go!");
+		System.out.println("Winnings: "+ total + " and Total Rooms: " + rooms);
 		in.close();
 	}
 
