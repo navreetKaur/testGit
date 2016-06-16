4cee3da5a25686fb73c82d883553f8e6bdff73e2
navreetKaur <navreet.kaur10@gmail.com>
2016-06-16 15:39:33 -0400

    updating exception

--- src/ExceptionHandling/Division.java
+++ src/ExceptionHandling/Division.java
@@ -11,15 +11,16 @@ public class Division {
 		  System.out.println("Input two integers");
 		 
 		  a = input.nextInt();		  
+
+		  b = input.nextInt();
 		  try {
-			  b = input.nextInt();
 			  if (b==0) {
-				  throw new Exception("Division by zero not possible");
+				  throw new ArithmeticException("Division by zero not possible");
 			  }
 			  result = a / b; 
 			  System.out.println("Result = " + result);
 			  input.close();
-		  } catch (Exception e) {
+		  } catch (ArithmeticException e) {
 			  System.out.println(e.getMessage());
 		  } finally {
 			input.close();
