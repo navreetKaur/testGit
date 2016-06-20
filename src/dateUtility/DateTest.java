a5d37ae7228cbf9de8288715ed609482d0e0f4f2
navreetKaur <navreet.kaur10@gmail.com>
2016-06-20 14:48:21 -0400

    Abstract

--- src/dateUtility/DateTest.java
+++ src/dateUtility/DateTest.java
@@ -9,7 +9,11 @@ public class DateTest {
 		System.out.println(DateUtility.getDaysInMonth(10, 2015));
 		System.out.println(DateUtility.isLeapYear(2016));
 		System.out.println(DateUtility.toString(8, 10, 2020, '/'));
-
+		System.out.println(DateUtility.dayOfTheWeek("1/30/2016"));
+		System.out.println(DateUtility.dayOfTheWeek(1,30,2016));
+		System.out.println(DateUtility.daysBwDates(1,30,2015));
+		System.out.println(DateUtility.daysBwDates("1/30/2015", "8/31/2016"));
+		System.out.println(DateUtility.daysBwDates(1, 30, 2015, 8, 31, 2016));
 	}
 
 }
--- src/dateUtility/DateUtility.java
+++ src/dateUtility/DateUtility.java
@@ -1,8 +1,11 @@
 package dateUtility;
 
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Locale;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class DateUtility {
 	
@@ -64,6 +67,71 @@ public class DateUtility {
 
 	public static boolean isLeapYear(int year) {
 		return ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0);
+	}
+	
+	public static int dayOfTheWeek(int month, int day, int year) {
+		Calendar cal = new GregorianCalendar(year,month,day);
+		return cal.get(Calendar.DAY_OF_WEEK);
+	}
+	public static int dayOfTheWeek(String date) {
+		Scanner dateParse = new Scanner(date);
+		dateParse.useDelimiter("/");
+		int month = (dateParse.nextInt());
+		int day = (dateParse.nextInt());
+		int year = (dateParse.nextInt());
+		dateParse.close();
+		return dayOfTheWeek(month,day,year);
+	}
+	public static int daysBwDates(String date1, String date2) {
+		Scanner dateParse = new Scanner(date1);
+		dateParse.useDelimiter("/");
+		int month = (dateParse.nextInt());
+		int day = (dateParse.nextInt());
+		int year = (dateParse.nextInt());
+		dateParse.close();
+		
+		Scanner dateParse1 = new Scanner(date2);
+		dateParse1.useDelimiter("/");
+		int month1 = (dateParse1.nextInt());
+		int day1 = (dateParse1.nextInt());
+		int year1 = (dateParse1.nextInt());
+		dateParse1.close();
+		return daysBwDates(month,day,year,month1,day1,year1);
+	}
+	public static int daysBwDates(int month,int day,int year ) {
+		Calendar cal1 = new GregorianCalendar();
+		int month1 = cal1.get(GregorianCalendar.MONTH)+1;
+		int day1 = cal1.get(GregorianCalendar.DAY_OF_MONTH);
+		int year1 = cal1.get(GregorianCalendar.YEAR);
+		return daysBwDates(month,day,year,month1,day1,year1);
+	}
+	public static int daysBwDates(int month, int day, int year, int month1, int day1, int year1) {
+		Calendar cal1 = new GregorianCalendar(year,month,day);
+		Calendar cal2 = new GregorianCalendar(year,month,day);
+		Calendar dayOne = (Calendar) cal1.clone(),
+	            dayTwo = (Calendar) cal2.clone();
+		long DAY_IN_MILLIS = 1000 * 60 * 60 * 24;
+		return ((int) ((cal1.getTime()) - cal2.getTime())/ DAY_IN_MILLIS));
+	    if (dayOne.get(Calendar.YEAR) == dayTwo.get(Calendar.YEAR)) {
+	        return Math.abs(dayOne.get(Calendar.DAY_OF_YEAR) - dayTwo.get(Calendar.DAY_OF_YEAR));
+	    } else {
+	        if (dayTwo.get(Calendar.YEAR) > dayOne.get(Calendar.YEAR)) {
+	            Calendar temp = dayOne;
+	            dayOne = dayTwo;
+	            dayTwo = temp;
+	        }
+	        int extraDays = 0;
+
+	        int dayOneOriginalYearDays = dayOne.get(Calendar.DAY_OF_YEAR);
+
+	        while (dayOne.get(Calendar.YEAR) > dayTwo.get(Calendar.YEAR)) {
+	            dayOne.add(Calendar.YEAR, -1);
+	            // getActualMaximum() important for leap years
+	            extraDays += dayOne.getActualMaximum(Calendar.DAY_OF_YEAR);
+	        }
+
+	        return extraDays - dayTwo.get(Calendar.DAY_OF_YEAR) + dayOneOriginalYearDays ;
+	    }
 	}
 
 }
